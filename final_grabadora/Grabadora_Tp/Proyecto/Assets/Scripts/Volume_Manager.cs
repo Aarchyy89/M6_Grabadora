48d41a386cd736e5c07084ea1a40419104b5cd61
Aarchyy89 <abarrios@santmarc.com>
2023-05-09 15:00:11 +0200

    8. Volumen general
    uejsrje

---
+++ final_grabadora/Grabadora_Tp/Proyecto/Assets/Scripts/Volume_Manager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volume_Manager : MonoBehaviour
+{
+    [Header("musica")] public Slider sliderMusica;
+    public float slidervalueMusica;
+    //public Image imgMuteMusica;
+    public AudioSource sourceMusica;
+
+    void Start()
+    {
+        sliderMusica.value = PlayerPrefs.GetFloat("volmenMusica", 0.5f);
+        sourceMusica.volume = sliderMusica.value;
+        //musicaMute();
+    }
+
+    public void slidermusica(float valorMusica)
+    {
+        slidervalueMusica = valorMusica;
+        PlayerPrefs.SetFloat("volmenMusica", slidervalueMusica);
+        sourceMusica.volume = sliderMusica.value;
+        //musicaMute();
+
+    }
+
+
+    /**public void musicaMute()
+    {
+        if (slidervalueMusica == 0)
+        {
+            imgMuteMusica.enabled = true;
+        }
+        else
+        {
+            imgMuteMusica.enabled = false;
+        }
+    }*/
+
+}
